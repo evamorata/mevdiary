da5ff839cbae73c0ac5ae596b55e88e23759bdfb
emg <137726861+evamorata@users.noreply.github.com>
2024-02-14 03:09:38 +0100

    Add files via upload

---
+++ js/stars1.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const numStars = 100; // Número de estrellas que deseas generar
+
+    const starsContainer = document.querySelector('.stars');
+
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        star.style.left = `${Math.random() * 100}vw`; // Posición horizontal aleatoria
+        star.style.top = `${Math.random() * 100}vh`; // Posición vertical aleatoria
+        star.style.animationDelay = `${Math.random() * 10}s`; // Retraso de animación aleatorio
+        starsContainer.appendChild(star);
+    }
+});
